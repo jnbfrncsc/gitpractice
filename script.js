168ed78809eb924c4b3a8fd197b84781339ce396
joan.francisco1 <joan.francisco1@trans-cosmos.co.jp>
2024-09-30 14:05:11 +0800

    Added Files

---
+++ script.js
@@ -0,0 +1,28 @@
+let http = new XMLHttpRequest();
+
+http.open('get', 'products.json', true);
+
+http.send();
+
+http.onload = function(){
+    if(this.readyState == 4 && this.status == 200){
+        let products = JSON.parse(this.responseText);
+        let output = "";
+
+        for(let item of products){
+            output += `
+                <div class="product">
+                    <img src="${item.image}" alt="${item.description}">
+                    <p class="title">${item.title}</p>
+                    <p class="description">${item.description}</p>
+                    <p class="price">
+                        <span>${item.price}</span>
+                        <span>€</span>
+                    </p>
+                    <p class="cart">Add to cart<i class="ri-shopping-cart-2-line"></i></p>
+                </div>
+            `;
+        }
+        document.querySelector(".products").innerHTML = output;
+    }
+}
